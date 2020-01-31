66afdabe440bd0fb7bf9e59a9105e4d3558a1f2a
Stephen H Bruen <52644685+stephenbruen@users.noreply.github.com>
2020-01-30 20:11:12 -0800

    Add files via upload

---
+++ hackerrank.java
@@ -0,0 +1,28 @@
+// HackerRank 1/30
+
+// Welcome to Java
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
+
+// Java Stdin and Stdout I
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+    }
+}
